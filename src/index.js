e1f074edc3d184334290e1b120d8ca5c646a40ec
haspadarZ-padMensku <gonchds@gmail.com>
2018-09-14 12:43:12 +0300

    implement solveEquation()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  equation = equation.replace(/\s|\*/g, '');
+  let arr = equation.split(/x\^2|x/g);
+
+  let a = +arr[0];
+  let b = +arr[1];
+  let c = +arr[2];
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  let x1 = Math.round((- b + Math.sqrt(d)) / (2 * a));
+  let x2 = Math.round((- b - Math.sqrt(d)) / (2 * a));
+
+  let res = [x1, x2];
+
+  return res.sort((a, b) => a - b);
 }
